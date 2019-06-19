ff7598209cbdac39474e319402f01b8120095280
DESKTOP-1GLKHLE\Damma <dammalapatimounica@gmail.com>
2019-06-18 22:13:44 -0600

    added class2 master

---
+++ ConsoleApp8test/AddClass2.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8test
+{
+    class AddClass2
+    {
+        //added class2
+    }
+}
